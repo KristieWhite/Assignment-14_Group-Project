366a72c3ea32fa3bbdfcbbf71a58a78adc1ad1a2
Kristie White <insanelynocturnal@gmail.com>
2015-10-29 23:29:54 -0700

    css updates

--- main.js
+++ main.js
@@ -28,28 +28,95 @@ $(document).ready(function(){
     var beerHTML = Mustache.render(beerTemplate, beerPull);
     $("#beerProducts").html(beerHTML);
 
-    var entreesItem = menu.entrees.map(function(obj){
-    return{
-        name:obj.item,
-        price: obj.price,
-        description: obj.description,
-        allergies: obj.allergies,
-        favorite:obj.favorite,
-        spicy:obj.spicy,
-        vegan:obj.vegan
+    var entreeItem = menu.entrees.map(function(obj) {
+        return {
+            name: obj.item,
+            price: obj.price,
+            description: obj.description,
+            vegan: obj.vegan,
+            spicy: obj.spicy,
+            favorite: obj.favorite
+        };
+    });
+
+    var entreePull = { "entreeItem": entreeItem};
+
+    var entreeTemplate = $("#entreeTemplate").text();
+    var entreeHTML = Mustache.render(entreeTemplate, entreePull);
+    $("#entreeProducts").html(entreeHTML);
 
 
-    };
-});
-    var entreesPull= {
-        "entreesItem":entreesItem
-    };
 
-    var entreesTemplate = $("entreesTemplate").text();
-    var entreesHTML = Mustache.render(entreesTemplate, entreesPull);
-    $("#entreesProducts").html(entreesHTML);
+    var gamesItem = menu.games.map(function(obj){
+        return{
+            name:obj.item,
+            price:obj.price,
+            description:obj.description,
+            rating:obj.rating,
+            platform:obj.platform
+        };
+    });
+
+    var gamesPull = { "gamesItem": gamesItem};
+
+    var gamesTemplate = $("#gamesTemplate").text();
+    var gamesHTML = Mustache.render(gamesTemplate,gamesPull);
+    $("#gamesProducts").html(gamesHTML);
 
 
+    }); //this is where the first ajax ends
+
+    var news = 'https://json-data.herokuapp.com/restaurant/news/1';
+
+    $.ajax({
+        url: news,
+        method: 'get',
+        dataType: 'json'
+    }).then(function(newsItem) {
+
+        var newsPull = {'newsItem': newsItem};
+
+        var newsTemplate = $("#newsTemplate").text();
+
+        var newsHTML = Mustache.render(newsTemplate, newsPull);
+
+        $("#newsProducts").html(newsHTML);
 
     });
+
+    var special = "https://json-data.herokuapp.com/restaurant/special/1";
+
+    $.ajax({
+        url: special,
+        method: 'get',
+        dataType: 'json'
+    }).then(function(specialItem){
+        var specialPull = {
+            'specialItem': specialItem
+        };
+        var specialTemplate = $("#specialTemplate").text();
+        var specialHTML = Mustache.render(specialTemplate, specialPull);
+        $("#specialProducts").html(specialHTML);
+
+    });
+
+    $(function () {
+   $(".menu div").show();
+   setTimeout("$('.menu div').slideToggle('fast');", 1);
+   $(".menu h3").click(function () {
+       $(this).next(".dropDown").slideToggle("fast").siblings(".dropDown:visible").slideUp("slow");
+       $(this).toggleClass("current");
+       $(this).siblings("h3").removeClass("current");
+   });
 });
+
+
+});
+
+
+
+
+
+
+
+
